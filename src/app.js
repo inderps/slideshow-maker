e086696e60e1e06f30efd4f0b470fd2e03b50da5
Indigo <indigo@INdigoProject.local>
2014-07-07 15:36:42 +0530

    basic looping

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 var SlideShow = {
     init: function(){
+        var _this = this;
+        this.slideCount = 0;
         $('body').keyup(function(e){
             if(e.keyCode == 32){
-                console.log('hello');
+                _this.slideCount = _this.slideCount + 1;
+                if(_this.slideCount <= SLIDEDATA.length){
+                    _this.renderSlide(_this.slideCount);
+                }
             }
         });
+    },
+
+    renderSlide: function(slideCount){
+        slideCount = slideCount - 1;
+        for(var i=0; i<SLIDEDATA[slideCount].length; i++){
+           this.renderElement(SLIDEDATA[slideCount][i]);
+        }
+    },
+
+    renderElement: function(data){
+        console.log(data);
     }
 }
 
